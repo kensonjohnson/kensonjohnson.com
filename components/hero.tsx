62afede971c6755c4acd09b79b5367a67d500e67
Kenson Johnson <kenson.johnson@outlook.com>
2023-09-07 21:03:49 -0500

    Adjusted heading and sub-heading content

--- components/hero.tsx
+++ components/hero.tsx
@@ -4,7 +4,7 @@ import HeroImage from "@/public/images/profile_square_hi_res.jpg";
 export default function Hero() {
   return (
     <section>
-      <div className="max-w-[700px]">
+      <div className="max-w-[800px]">
         <div className="pt-8 pb-10">
           <Image
             className="rounded-full mb-5"
@@ -15,16 +15,19 @@ export default function Hero() {
             alt="Me"
           />
           <h1 className="h1 font-aspekta mb-5">
-            I write about coding and being a{" "}
+            I write about{" "}
             <span className="inline-flex relative text-sky-500 before:absolute before:inset-0 before:bg-sky-200 dark:before:bg-sky-500 before:opacity-30 before:-z-10 before:-rotate-2 before:translate-y-1/4">
-              full-time
-            </span>{" "}
-            maker.
+              code
+            </span>
+            .
           </h1>
           <p className="text-lg text-slate-500 dark:text-slate-400">
-            Writer, Speaker, Developer, and Co-Founder of Code.co, and
-            AppForYou. I write about coding, startups, and my journey as a
-            full-time maker.
+            Developer, Writer and Co-Founder of{" "}
+            <a href="https://paperbullx.com" className="text-blue-500">
+              Paper Bull Trading
+            </a>
+            . I write about coding, startups, and my journey as a full-time
+            maker.
           </p>
         </div>
       </div>
