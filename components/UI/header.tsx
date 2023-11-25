5a4e7cf4220d13ac38b7f848f686851d64b6b070
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2023-11-25 15:06:36 -0600

    Changed CTA to contact

--- components/UI/header.tsx
+++ components/UI/header.tsx
@@ -1,5 +1,5 @@
-import Link from 'next/link'
-import ThemeToggle from './theme-toggle'
+import Link from "next/link";
+import ThemeToggle from "./theme-toggle";
 
 export default function Header() {
   return (
@@ -14,9 +14,17 @@ export default function Header() {
                   Search
                 </label>
                 <div className="relative flex items-center">
-                  <input id="search" type="search" className="form-input py-1 w-full pl-10" />
+                  <input
+                    id="search"
+                    type="search"
+                    className="form-input py-1 w-full pl-10"
+                  />
                   <div className="absolute inset-0 right-auto flex items-center justify-center">
-                    <svg className="w-4 h-4 shrink-0 mx-3" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+                    <svg
+                      className="w-4 h-4 shrink-0 mx-3"
+                      viewBox="0 0 16 16"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
                       <path
                         className="fill-slate-400"
                         d="M7 14c-3.86 0-7-3.14-7-7s3.14-7 7-7 7 3.14 7 7-3.14 7-7 7zM7 2C4.243 2 2 4.243 2 7s2.243 5 5 5 5-2.243 5-5-2.243-5-5-5zm8.707 12.293a.999.999 0 11-1.414 1.414L11.9 13.314a8.019 8.019 0 001.414-1.414l2.393 2.393z"
@@ -33,12 +41,15 @@ export default function Header() {
 
           {/* Button */}
           <div>
-            <Link className="btn-sm text-slate-100 bg-sky-500 hover:bg-sky-600" href="/subscribe">
-              Subscribe
+            <Link
+              className="btn-sm text-slate-100 bg-sky-500 hover:bg-sky-600"
+              href="/contact"
+            >
+              Contact
             </Link>
           </div>
         </div>
       </div>
     </header>
-  )
+  );
 }
