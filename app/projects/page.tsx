d3e2a2ce1c114bff0e854b76c917983a0c847c50
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2023-11-29 14:49:46 -0600

    Add components and content for Projects Page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,125 +1,102 @@
-import ProjectCard from "../project-card";
-import WidgetNewsletter from "@/components/widget-newsletter";
-import WidgetSponsor from "@/components/widget-sponsor";
+import ProjectCard from "@/components/Projects/ProjectCard";
 
-import Icon01 from "@/public/images/project-icon-01.svg";
-import Icon02 from "@/public/images/project-icon-02.svg";
-import Icon03 from "@/public/images/project-icon-03.svg";
-import Icon04 from "@/public/images/project-icon-04.svg";
-import Icon05 from "@/public/images/project-icon-05.svg";
-import Icon06 from "@/public/images/project-icon-06.svg";
-import Icon07 from "@/public/images/project-icon-07.svg";
-import Icon08 from "@/public/images/project-icon-08.svg";
+// Icon imports
+import CryptoRealtimeInferenceIcon from "@/public/images/crypto_realtime_inference_icon.svg";
+import AlgorithmVisualizerIcon from "@/public/images/algorithm_visualizer_icon.svg";
+import DelegateIcon from "@/public/images/delegate_icon.svg";
+import CKLogoLight from "@/public/images/ck_logo_light.svg";
+import CKLogoDark from "@/public/images/ck_logo_dark.svg";
+import UnderdogLogo from "@/public/images/underdog_devs_logo.png";
+// import GitHubIcon from "@/public/images/github.svg";
 
 export const metadata = {
-  title: "Projects - DevSpace",
-  description: "Page description",
+  title: "Projects - Kenson Johnson",
+  description: "All of the awesome projects I've built or worked on.",
 };
 
 export default function Projects() {
-  const items01 = [
+  const githubProjects = [
     {
-      id: 0,
-      icon: Icon01,
-      slug: "#0",
-      title: "Container Tinkering",
-      excerpt: "Solutions for running containers locally and remotely.",
-      openSource: true,
+      id: "crypto_realtime_inference",
+      iconLight: CryptoRealtimeInferenceIcon,
+      iconDark: CryptoRealtimeInferenceIcon,
+      link: "https://github.com/christopherkeim/crypto-real-time-inference",
+      demoLink: "https://www.christopherkeim.xyz/projects/crypto",
+      title: "Crypto Real Time Inference",
+      excerpt:
+        "Leverages historical Bitcoin price data and cutting-edge machine learning algorithms to serve inferences about Bitcoin's future price points within a 1 hour window, in real time.",
     },
     {
-      id: 0,
-      icon: Icon02,
-      slug: "#0",
-      title: "Engine Prototypes",
-      excerpt: "Solutions for running containers locally and remotely.",
-      openSource: false,
+      id: "algorithm_visualizer",
+      iconLight: AlgorithmVisualizerIcon,
+      iconDark: AlgorithmVisualizerIcon,
+      link: "https://github.com/olijujuangreen/Pathfinder-React",
+      demoLink: "https://olijujuangreen.github.io/Pathfinder-React/",
+      title: "Algorithm Visualizer",
+      excerpt:
+        "I built this app to use as a teaching tool for my mentees. It visualizes the inner workings of various pathfinding algorithms, such as Breadth first search and Dijkstra's algorithm.",
+    },
+    {
+      id: "delegate_app",
+      iconLight: DelegateIcon,
+      iconDark: DelegateIcon,
+      link: "https://github.com/creppyfm/DeleGate",
+      demoLink: "https://delegate-ai.herokuapp.com/",
+      title: "Delegate App",
+      excerpt:
+        "An AI powered project management tool that helps managers delegate tasks to their team members.",
     },
   ];
 
-  const items02 = [
+  const clientProjects = [
     {
-      id: 0,
-      icon: Icon03,
-      slug: "#0",
-      title: "PixelOkay",
-      excerpt: "Code assets and services for people, with people.",
-      openSource: false,
+      id: "christopher_keim_website",
+      iconLight: CKLogoLight,
+      iconDark: CKLogoDark,
+      link: "https://github.com/christopherkeim/christopherkeim-xyz",
+      websiteLink: "https://www.christopherkeim.xyz",
+      title: "Christopher Keim's Portfolio Website",
+      excerpt: "Next 14, Tailwind CSS, TypeScript, and Vercel.",
     },
     {
-      id: 1,
-      icon: Icon04,
-      slug: "#0",
-      title: "Storybook",
-      excerpt: "Storybook helps you develop, test, and document UIs.",
-      openSource: false,
-    },
-    {
-      id: 2,
-      icon: Icon05,
-      slug: "#0",
-      title: "Knowledge AI",
-      excerpt:
-        "Instantly answers all questions based on your internal knowledge bases.",
-      openSource: false,
-    },
-    {
-      id: 3,
-      icon: Icon06,
-      slug: "#0",
-      title: "Security Frame",
-      excerpt: "Automated security compliance for your business.",
-      openSource: false,
-    },
-    {
-      id: 4,
-      icon: Icon07,
-      slug: "#0",
-      title: "KanbanOK",
-      excerpt: "The most powerful kanban board ever invented.",
-      openSource: false,
-    },
-    {
-      id: 5,
-      icon: Icon08,
-      slug: "#0",
-      title: "T Analytics",
-      excerpt: "Make your Twitter analytics pretty and easy to share.",
-      openSource: false,
+      id: "underdog_devs_website",
+      iconLight: UnderdogLogo,
+      iconDark: UnderdogLogo,
+      link: "https://github.com/Underdog-Devs/ud-client-v3",
+      websiteLink: "https://www.underdogdevs.org",
+      title: "Underdog Devs Website",
+      excerpt: "Next 13, Sass, TypeScript, and AWS.",
     },
   ];
 
   return (
-    <div className="grow md:flex space-y-8 md:space-y-0 md:space-x-8 pt-12 md:pt-16 pb-16 md:pb-20">
-      {/* Middle area */}
+    <div className="relative mx-auto max-w-6xl px-4 pb-12 pt-32 sm:px-6 md:pb-20">
       <div className="grow">
-        <div className="max-w-[800px]">
+        <div className="w-full">
           <section>
             {/* Page title */}
             <h1 className="h1 font-aspekta mb-12">
               Nice stuff I&apos;ve built
             </h1>
-            {/* Page content */}
             <div className="space-y-10">
-              {/* Side Hustles cards */}
+              {/* Github projects cards */}
               <section>
-                <h2 className="font-aspekta text-xl font-[650] mb-6">
-                  Side Hustles
+                <h2 className="font-aspekta mb-6 text-xl font-[650]">
+                  Projects
                 </h2>
-                {/* Cards */}
-                <div className="grid sm:grid-cols-2 md:grid-cols-1 lg:grid-cols-2 gap-5">
-                  {items01.map((item) => (
+                <div className="grid gap-5 md:grid-cols-2">
+                  {githubProjects.map((item) => (
                     <ProjectCard key={item.id} item={item} />
                   ))}
                 </div>
               </section>
               {/* Client Projects cards */}
               <section>
-                <h2 className="font-aspekta text-xl font-[650] mb-6">
+                <h2 className="font-aspekta mb-6 text-xl font-[650]">
                   Client Projects
                 </h2>
-                {/* Cards */}
-                <div className="grid sm:grid-cols-2 md:grid-cols-1 lg:grid-cols-2 gap-5">
-                  {items02.map((item) => (
+                <div className="grid gap-5 md:grid-cols-2">
+                  {clientProjects.map((item) => (
                     <ProjectCard key={item.id} item={item} />
                   ))}
                 </div>
@@ -128,14 +105,6 @@ export default function Projects() {
           </section>
         </div>
       </div>
-
-      {/* Right sidebar */}
-      <aside className="md:w-[240px] lg:w-[300px] shrink-0">
-        <div className="space-y-6">
-          <WidgetNewsletter />
-          <WidgetSponsor />
-        </div>
-      </aside>
     </div>
   );
 }
